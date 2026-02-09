99cc6e3bc2770e8d24f52d68dd6cde0f5e2177d2
Mihail Perevertkin <mihail.perevertkin@apm-consult.com>
2026-02-09 15:51:05 +0300

    ABA-9 add analitycs

--- apps/mobile/src/components/charts/index.ts
+++ apps/mobile/src/components/charts/index.ts
@@ -1,2 +1,5 @@
 export { BarChart } from './BarChart';
 export { PieChart } from './PieChart';
+export { DonutChart } from './DonutChart';
+export { GroupedBarChart } from './GroupedBarChart';
+export { WeekdayChart } from './WeekdayChart';
