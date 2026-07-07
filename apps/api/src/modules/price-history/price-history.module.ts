c9a67bd2b701d17a41955f3d441727b2c30cac7d
Mihail Perevertkin <perevertkinma@gmail.com>
2026-07-07 21:07:58 +0300

    feat(price-history): add Pro-gated POST /basket comparison endpoint

--- apps/api/src/modules/price-history/price-history.module.ts
+++ apps/api/src/modules/price-history/price-history.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PriceHistoryController } from './price-history.controller';
 import { PriceHistoryService } from './price-history.service';
+import { SubscriptionsModule } from '../subscriptions/subscriptions.module';
 
 @Module({
+  imports: [SubscriptionsModule],
   controllers: [PriceHistoryController],
   providers: [PriceHistoryService],
 })
